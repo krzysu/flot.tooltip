de8c4fc5481fca3862b0eeac8a73727837b5ee04
Krzysztof Urbas <krzysu@interia.pl>
2014-03-26 15:01:15 +0100

    add polyfill for array.indexOf for IE8

--- js/jquery.flot.tooltip.js
+++ js/jquery.flot.tooltip.js
@@ -6,9 +6,37 @@
  * author: Krzysztof Urbas @krzysu [myviews.pl]
  * website: https://github.com/krzysu/flot.tooltip
  * 
- * build on 2014-03-14
+ * build on 2014-03-26
  * released under MIT License, 2012
 */ 
+// IE8 polyfill for Array.indexOf
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement, fromIndex) {
+        if ( this === undefined || this === null ) {
+            throw new TypeError( '"this" is null or not defined' );
+        }
+        var length = this.length >>> 0; // Hack to convert object.length to a UInt32
+        fromIndex = +fromIndex || 0;
+        if (Math.abs(fromIndex) === Infinity) {
+            fromIndex = 0;
+        }
+        if (fromIndex < 0) {
+            fromIndex += length;
+            if (fromIndex < 0) {
+                fromIndex = 0;
+            }
+        }
+
+        for (;fromIndex < length; fromIndex++) {
+            if (this[fromIndex] === searchElement) {
+                return fromIndex;
+            }
+        }
+
+        return -1;
+    };
+}
+
 (function ($) {
 
     // plugin options, default values
